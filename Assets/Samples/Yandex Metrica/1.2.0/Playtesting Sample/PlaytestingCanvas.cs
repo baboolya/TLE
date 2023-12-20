fc9cacbca1c650c53cdc8bc1f5626964825c443d
baboolya <150634483+baboolya@users.noreply.github.com>
2023-12-20 18:49:36 +0700

    45

---
+++ Assets/Samples/Yandex Metrica/1.2.0/Playtesting Sample/PlaytestingCanvas.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Agava.YandexMetrica.Samples
+{
+    public class PlaytestingCanvas : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField _eventNameField;
+        [SerializeField]
+        private InputField _eventDataField;
+
+        private void Awake()
+        {
+            YandexMetrica.CallbackLogging = true;
+        }
+
+        public void OnSendButtonClick()
+        {
+            YandexMetrica.Send(_eventNameField.text, _eventDataField.text);
+        }
+    }
+}
