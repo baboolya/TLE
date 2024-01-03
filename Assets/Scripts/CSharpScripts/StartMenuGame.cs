78b7ce7a10d638e972fe59d0f939d902d68dc247
baboolya <kaka.20001@mail.ru>
2024-01-03 10:58:09 +0700

    returnAgavaInProject

--- Assets/Scripts/CSharpScripts/StartMenuGame.cs
+++ Assets/Scripts/CSharpScripts/StartMenuGame.cs
@@ -2,27 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Naninovel;
-using YG;
 
 public class StartMenuGame : MonoBehaviour
 {
-    private void OnEnabled()
-    {
-        YandexGame.GetDataEvent += GetData;
-    }
-    
-    private void OnDisabled()
-    {
-        YandexGame.GetDataEvent -= GetData;
-    }
-
     private void Start()
-    {
-        if (YandexGame.SDKEnabled == true)
-            GetData();
-    }
-
-    private void GetData()
     {
         RuntimeInitializer.InitializeAsync();
     }
